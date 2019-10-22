a6e54d89aa937e1f3479698112a18fe1e878fae7
LancelotGD <35948261+LancelotGD@users.noreply.github.com>
2019-10-22 13:04:14 +0800

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,234 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import time
+
+# This is the two-step process used to prepare the
+# data for use with the convolutional neural network.
+
+# First step is to convert Python Image Library (PIL) format
+# to PyTorch tensors.
+
+# Second step is used to normalize the data by specifying a
+# mean and standard deviation for each of the three channels.
+# This will convert the data from [0,1] to [-1,1]
+
+# Normalization of data should help speed up conversion and
+# reduce the chance of vanishing gradients with certain
+# activation functions.
+begin=time.time()
+device=("cuda" if torch.cuda.is_available() else "cpu")
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./dataset/',
+                                        train=True,
+                                        download=False,
+                                        transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=4,
+                                          shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(root='./dataset/',
+                                       train=False,
+                                       download=False,
+                                       transform=transform)
+testloader = torch.utils.data.DataLoader(testset,
+                                         batch_size=4,
+                                         shuffle=False)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+import matplotlib.pyplot as plt
+import numpy as np
+
+def convert_to_imshow_format(image):
+    # first convert back to [0,1] range from [-1,1] range
+    image = image / 2 + 0.5
+    image = image.numpy()
+    # convert from CHW to HWC
+    # from 3x32x32 to 32x32x3
+    return image.transpose(1,2,0)
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+fig, axes = plt.subplots(1, len(images), figsize=(12,2.5))
+for idx, image in enumerate(images):
+    axes[idx].imshow(convert_to_imshow_format(image))
+    axes[idx].set_title(classes[labels[idx]])
+    axes[idx].set_xticks([])
+    axes[idx].set_yticks([])
+import torch.nn as nn
+import torch.nn.functional as F
+
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                     stride=stride, padding=1, bias=False)
+
+class ResidualBlock(nn.Module):#残差网络
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+class Net(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super(Net, self).__init__()
+        self.in_channels = 16
+        self.conv = conv3x3(3, 16)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self.make_layer(block, 16, layers[0])
+        self.layer2 = self.make_layer(block, 32, layers[0], 2)
+        self.layer3 = self.make_layer(block, 64, layers[1], 2)
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64, num_classes)
+
+    def make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(out_channels))
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+net = Net(ResidualBlock, [2, 2, 2, 2]).to(device)
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+#optimizer = optim.Adam(net.parameters(), lr=0.001)
+import os
+
+model_directory_path = './dataset/'
+model_path = model_directory_path + 'cifar-10-cnn-model.pt'
+
+if not os.path.exists(model_directory_path):
+    os.makedirs(model_directory_path)
+
+if os.path.isfile(model_path):
+    # load trained model parameters from disk
+    net.load_state_dict(torch.load(model_path))
+    print('Loaded model parameters from disk.')
+else:
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data
+            inputs, labels = inputs.to(device),labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f ' %
+                      (epoch + 1, i + 1, running_loss / 2000),time.time()-begin)
+                running_loss = 0.0
+    print('Finished Training.')
+    torch.save(net.state_dict(), model_path)
+    print('Saved model parameters to disk.')
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+fig, axes = plt.subplots(1, len(images), figsize=(12,2.5))
+for idx, image in enumerate(images):
+    axes[idx].imshow(convert_to_imshow_format(image))
+    axes[idx].set_title(classes[labels[idx]])
+    axes[idx].set_xticks([])
+    axes[idx].set_yticks([])
+outputs = net(images)
+sm = nn.Softmax(dim=1)
+sm_outputs = sm(outputs)
+print(sm_outputs)
+probs, index = torch.max(sm_outputs, dim=1)
+
+for p, i in zip(probs, index):
+    print('{0} - {1:.4f}'.format(classes[i], p))
+total_correct = 0
+total_images = 0
+confusion_matrix = np.zeros([10,10], int)
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        images, labels = images.to(device),labels.to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total_images += labels.size(0)
+        total_correct += (predicted == labels).sum().item()
+        for i, l in enumerate(labels):
+            confusion_matrix[l.item(), predicted[i].item()] += 1
+
+model_accuracy = total_correct / total_images * 100
+print('Model accuracy on {0} test images: {1:.2f}%'.format(total_images, model_accuracy))
+print('{0:10s} - {1}'.format('Category','Accuracy'))
+for i, r in enumerate(confusion_matrix):
+    print('{0:10s} - {1:.1f}'.format(classes[i], r[i]/np.sum(r)*100))
+fig, ax = plt.subplots(1,1,figsize=(8,6))
+ax.matshow(confusion_matrix, aspect='auto', vmin=0, vmax=1000, cmap=plt.get_cmap('Blues'))
+plt.ylabel('Actual Category')
+plt.yticks(range(10), classes)
+plt.xlabel('Predicted Category')
+plt.xticks(range(10), classes)
+plt.show()
+print('actual/pred'.ljust(16), end='')
+for i, c in enumerate(classes):
+    print(c.ljust(10), end='')
+print()
+for i, r in enumerate(confusion_matrix):
+    print(classes[i].ljust(16), end='')
+    for idx, p in enumerate(r):
+        print(str(p).ljust(10), end='')
+    print()
+
+    r = r / np.sum(r)
+    print(''.ljust(16), end='')
+    for idx, p in enumerate(r):
+        print(str(p).ljust(10), end='')
+    print()
